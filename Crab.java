f74ddbda00f6ab4a8ba12b4e35bd51d443f98326
Jillian Maher <jillian.maher2@boone.kyschools.us>
2021-03-22 12:28:31 -0400

    Add objects to the world

--- Crab.java
+++ Crab.java
@@ -5,6 +5,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Crab extends Actor
 {
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+    
+    /*
+     * Create a Crab and initialize two image variables
+     */
+    public Crab() 
+    {
+        image1 = new GreenfootImage( "crab.png" );
+        image2 = new GreenfootImage( "crab2.png" );
+        setImage( image1 );
+    }
+    
     public void act()
     {
         checkKeyPress();
--- CrabWorld.java
+++ CrabWorld.java
@@ -9,5 +9,21 @@ public class CrabWorld extends World
     public CrabWorld() 
     {
         super(560, 560, 1);
+        addObject( new Crab(), 300, 300);
+        addObject( new Lobster(), 90, 70);
+        addObject( new Lobster(), 390, 200);
+        addObject( new Lobster(), 360, 500);
+        
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        addObject( new Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        
     }
 }
